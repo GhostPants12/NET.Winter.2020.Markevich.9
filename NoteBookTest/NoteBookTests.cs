c7743d1d68c8c6da9dd4a8058a975060ac511aac
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-02-09 17:36:13 +0300

    fix: remove operators overloading

--- NoteBookTest/NoteBookTests.cs
+++ NoteBookTest/NoteBookTests.cs
@@ -6,25 +6,6 @@ namespace NoteBookTest
     public class Tests
     {
         [Test]
-        public void NoteTests_ComparisonOperators()
-        {
-            Note firstNote = new Note("123");
-            Note firstNoteCopy = firstNote;
-            Note nullNote = null;
-            Note nullNoteCopy = nullNote;
-            Note secondNote = new Note("123");
-            Assert.AreEqual(false, firstNote < secondNote);
-            Assert.AreEqual(false, secondNote > firstNote);
-            Assert.AreEqual(false, firstNote <= secondNote);
-            Assert.AreEqual(false, secondNote >= firstNote);
-            Assert.AreEqual(false, firstNote == secondNote);
-            Assert.AreEqual(true, firstNote != secondNote);
-            Assert.AreEqual(true, firstNoteCopy == firstNote);
-            Assert.AreEqual(false, firstNote == nullNote);
-            Assert.AreEqual(true, nullNote == nullNoteCopy);
-        }
-
-        [Test]
         public void NoteTests_Equals()
         {
             Note firstNote = new Note("123");
@@ -54,7 +35,7 @@ namespace NoteBookTest
             bool notesAreEqual = true;
             for (int i = 0; i < 3; i++)
             {
-                if (noteBook[i] != expected[i])
+                if (!(noteBook[i].Equals(expected[i])))
                 {
                     notesAreEqual = false;
                 }
@@ -65,7 +46,7 @@ namespace NoteBookTest
             noteBook.Remove(firstNote);
             for (int i = 0; i < 2; i++)
             {
-                if (noteBook[i] != expected[i])
+                if (!(noteBook[i].Equals(expected[i])))
                 {
                     notesAreEqual = false;
                 }
@@ -74,7 +55,7 @@ namespace NoteBookTest
             Assert.IsTrue(notesAreEqual);
             expected.RemoveAt(1);
             noteBook.RemoveAt(1);
-            if (noteBook[0] != expected[0])
+            if (!(noteBook[0].Equals(expected[0])))
             {
                 notesAreEqual = false;
             }
@@ -89,13 +70,13 @@ namespace NoteBookTest
             Note secondNote = new Note("123");
             Note thirdNote = new Note("123");
             NoteBookService.Instance.AddNote(firstNote);
-            Assert.AreEqual(true, NoteBookService.Instance[0] == firstNote);
+            Assert.AreEqual(true, NoteBookService.Instance[0].Equals(firstNote));
             NoteBookService.Instance.RemoveNoteAt(0);
             NoteBookService.Instance.AddNote(secondNote);
-            Assert.AreEqual(true, NoteBookService.Instance[0] == secondNote);
+            Assert.AreEqual(true, NoteBookService.Instance[0].Equals(secondNote));
             NoteBookService.Instance.RemoveNote(secondNote);
             NoteBookService.Instance.AddNote(thirdNote);
-            Assert.AreEqual(true, NoteBookService.Instance[0] == thirdNote);
+            Assert.AreEqual(true, NoteBookService.Instance[0].Equals(thirdNote));
         }
     }
 }
